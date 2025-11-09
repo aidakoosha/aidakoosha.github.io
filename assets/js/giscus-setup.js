5acecca4faf733586534f58305b0ac5e16cd5b8c
aidakoosha <93160007+aidakoosha@users.noreply.github.com>
2025-11-09 18:32:48 +0000

    Deploying to gh-pages from @ aidakoosha/aidakoosha.github.io@44d71e42cb53d9d7c47bc16dca9c1f5d8a7b2da4 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "aidatar/aidatar.github.io",
+    "data-repo": "aidakoosha/aidakoosha.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
